0379c425cb991b9cf9d37581bba9a6dd524ee469
Emmanuel Ariza <emmanuel@debian.debian>
2017-11-09 16:55:29 -0500

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name buscadorVideosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the buscadorVideosApp
+ */
+angular.module('buscadorVideosApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name buscadorVideosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the buscadorVideosApp
+ */
+angular.module('buscadorVideosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
