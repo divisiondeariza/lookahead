4ef367de97282c4484707fc0751a981264e8b809
Emmanuel Ariza <emmanuel@debian.debian>
2017-11-14 16:52:20 -0500

    added lookahead for search field

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,18 +7,30 @@
  * # MainCtrl
  * Controller of the buscadorVideosApp
  */
-angular.module('buscadorVideosApp')
-  .controller('MainCtrl', ["$scope", "$http", "fCsv", function ($scope, $http, fCsv) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-	    $scope.selected=undefined;
-	    $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-		$http.get('../../data/videos.csv').then(function(resp) {
-		$scope.videos = fCsv.toJson(resp.data);
-	});
- 
+ angular.module('buscadorVideosApp')
+ .controller('MainCtrl', ["$scope", "$http", "fCsv", function ($scope, $http, fCsv) {
 
-  }]);
+ 	$scope.selected=undefined;
+ 	$scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+ 	$http.get('/../../data/videos.csv').then(function(resp) {
+
+ 		//$scope.videos = fCsv.toJson(resp.data,",",false);
+ 		$scope.videos = resp.data.split("\n")
+ 							.map(function(row){
+ 								return row.split(",")
+ 							});
+ 		var rawTags = []
+ 		$scope.videos.forEach(function(row){
+ 			console.log(row);
+ 			rawTags = rawTags.concat(row.slice(1));
+ 		})
+ 		
+ 		$scope.tags = rawTags.filter(function(item, pos, self) {
+ 										console.log(item);
+									    return item != "" && self.indexOf(item) == pos;
+									});
+ 		console.log($scope.tags);
+ 	});
+
+
+ }]);
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,12 +1,29 @@
 'use strict';
 
 describe('Controller: MainCtrl', function () {
+  var $httpBackend, authRequestHandler, mockResponse;
+  var MainCtrl, scope;
+
 
   // load the controller's module
   beforeEach(module('buscadorVideosApp'));
 
-  var MainCtrl,
-    scope;
+
+  beforeEach(inject(function($injector) {
+    mockResponse = "link1.com,tag1,tag2,\nlink2.com,tag1,tag3,tag4";
+
+    $httpBackend = $injector.get('$httpBackend');
+    authRequestHandler = $httpBackend
+      .whenRoute('GET', '/../../data/videos.csv')
+      .respond(function(method, url, data, headers, params) {
+        return [200, mockResponse];
+      });
+  }));
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
@@ -17,7 +34,9 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(MainCtrl.awesomeThings.length).toBe(3);
+  it('Populates scope,tags with the tags', function () {
+    $httpBackend.expectGET('/../../data/videos.csv');
+    $httpBackend.flush();
+    expect(scope.tags).toEqual(["tag1", "tag2", "tag3", "tag4"]);
   });
 });
