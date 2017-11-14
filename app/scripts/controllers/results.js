e81a3af16322693d1eaa0d63ff1776d3edb93aef
Emmanuel Ariza <emmanuel@debian.debian>
2017-11-14 17:35:55 -0500

    the results view was created and the main page search button redirect to it

---
+++ app/scripts/controllers/results.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name buscadorVideosApp.controller:ResultsCtrl
+ * @description
+ * # ResultsCtrl
+ * Controller of the buscadorVideosApp
+ */
+angular.module('buscadorVideosApp')
+  .controller('ResultsCtrl', function () {
+
+  });
